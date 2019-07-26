c3509e983c9e7395f0cf8e316f2c4c0c1f86d2db
Luc Deuffic <ldeuffic@afi-sa.fr>
2019-07-26 16:25:19 +0200

    Commit CT bokeh-php : modif Dockerfile et scripts déploiement. Test CT OK

---
+++ bokeh-php/start-php.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function main {
+	su -l webmaster -c "bash /tmp/init-php.sh ${install_dir}"
+
+	if [ -z "$(ls -A /home/webmaster/${install_dir}/conf/php)" ]
+	then
+		echo "[BOKEH-PHP] Install PHP conf files..."
+		mv /tmp/bokeh-php-fpm.conf /home/webmaster/${install_dir}/conf/php/
+		mv /tmp/bokeh-pool.conf /home/webmaster/${install_dir}/conf/php/
+		sed -i "s:INSTALLDIR:${install_dir}:g" /home/webmaster/${install_dir}/conf/php/bokeh-php-fpm.conf
+		sed -i "s:INSTALLDIR:${install_dir}:g" /home/webmaster/${install_dir}/conf/php/bokeh-pool.conf
+	fi
+
+	sed -i "s:INSTALLDIR:${install_dir}:g" /usr/local/etc/php/php.ini
+	sed -i "s:INSTALLDIR:${install_dir}:g" /usr/local/etc/php-fpm.conf
+
+	echo "[BOKEH-PHP] Start php-fpm..."
+	php-fpm -F
+}
+
+main
