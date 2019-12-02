8a52ed28ed8e3edda5844f73e2f663d50f04285e
Luc Deuffic <ldeuffic@afi-sa.fr>
2019-12-02 14:12:54 +0100

    Harmonization of start|init-php.sh scripts between Prod and Dev PHP CTs.

--- bokeh-php-prod/init-php.sh
+++ bokeh-php-prod/init-php.sh
@@ -1,48 +1,86 @@
 #!/bin/bash
 
-function main {
-	if [ ! -d /home/webmaster/${install_dir} ]
-	then
-		echo "[BOKEH-PHP] Create bokeh subtree..."
-		mkdir -p /home/webmaster/${install_dir}/php/bokeh
-		mkdir -p /home/webmaster/${install_dir}/conf/php
-		mkdir -p /home/webmaster/${install_dir}/conf/mysql
-		mkdir -p /home/webmaster/${install_dir}/conf/nginx
-		mkdir -p /home/webmaster/${install_dir}/log/php
-		mkdir -p /home/webmaster/${install_dir}/log/nginx
-		mkdir -p /home/webmaster/${install_dir}/log/mysql
-		mkdir -p /home/webmaster/${install_dir}/htdocs
-		mkdir -p /home/webmaster/${install_dir}/ftp
-		mkdir -p /home/webmaster/${install_dir}/mysql
-		mkdir -p /home/webmaster/${install_dir}/scripts
-		mkdir -p /home/webmaster/${install_dir}/temp
-		chmod 777 /home/webmaster/${install_dir}/temp
-	fi
-
-	if [ ! -a /home/webmaster/${install_dir}/php/install-util.sh ]
-	then
-		cp /tmp/install-util.sh /home/webmaster/${install_dir}/php/
-		chown webmaster:webmaster /home/webmaster/${install_dir}/php/*.sh
-	fi
-
-	if [ -z "$(ls -A /home/webmaster/${install_dir}/php/bokeh)" ]
-	then
-		echo "[BOKEH-PHP] Install source tree..."
-		cd /home/webmaster/${install_dir}/php/bokeh
-		git clone https://git.afi-sa.net/afi/opacce.git .
-	fi
-
-	cd /home/webmaster/${install_dir}/php/bokeh
-	source /home/webmaster/${install_dir}/php/install-util.sh
-	nextStep "git reset --hard HEAD"
-	executeCommand "git reset --hard HEAD"
-	nextStep "git fetch -t --all"
-	executeCommand "git fetch -t --all"
-	nextStep "git checkout $(git describe --tags)"
-	executeCommand "git checkout $(git describe --tags)"
-	nextStep "./update.sh"
-	executeCommand "./update.sh"
+function ensure_directories {
+    if [ ! -d /home/webmaster/${install_dir} ]
+    then
+	echo "[BOKEH-PHP] Create bokeh subtree..."
+	mkdir -p /home/webmaster/${install_dir}/php/bokeh
+	mkdir -p /home/webmaster/${install_dir}/conf/php
+	mkdir -p /home/webmaster/${install_dir}/conf/mysql
+	mkdir -p /home/webmaster/${install_dir}/conf/nginx
+	mkdir -p /home/webmaster/${install_dir}/log/php
+	mkdir -p /home/webmaster/${install_dir}/log/nginx
+	mkdir -p /home/webmaster/${install_dir}/log/mysql
+	mkdir -p /home/webmaster/${install_dir}/htdocs
+	mkdir -p /home/webmaster/${install_dir}/ftp
+	mkdir -p /home/webmaster/${install_dir}/mysql
+	mkdir -p /home/webmaster/${install_dir}/scripts
+	mkdir -p /home/webmaster/${install_dir}/temp
+	chmod 777 /home/webmaster/${install_dir}/temp
+    fi
 }
 
+
+function ensure_install_utils {
+    if [ ! -a /home/webmaster/${install_dir}/php/install-util.sh ]
+    then
+        cp /tmp/install-util.sh /home/webmaster/${install_dir}/php/
+        chown webmaster:webmaster /home/webmaster/${install_dir}/php/*.sh
+    fi
+    source /home/webmaster/${install_dir}/php/install-util.sh
+}
+
+
+function ensure_bokeh_sources {
+    if [ -z "$(ls -A /home/webmaster/${install_dir}/php/bokeh)" ]
+    then
+        nextStep "[BOKEH-PHP] Install source tree..."
+        cd /home/webmaster/${install_dir}/php/bokeh
+	git clone https://git.afi-sa.net/afi/opacce.git .
+    fi
+
+    cd /home/webmaster/${install_dir}/php/bokeh
+    nextStep "./update.sh"
+    executeCommand "./update.sh"
+}
+
+
+function ensure_confs {
+    if [ -z "$(ls -A /home/webmaster/${install_dir}/conf/php)" ]
+    then
+        echo "[BOKEH-PHP] Install PHP conf files..."
+        cp -p /tmp/bokeh-php-fpm.conf /home/webmaster/${install_dir}/conf/php/
+        cp -p /tmp/bokeh-pool.conf /home/webmaster/${install_dir}/conf/php/
+        sed -i "s:INSTALLDIR:${install_dir}:g" /home/webmaster/${install_dir}/conf/php/bokeh-php-fpm.conf
+        sed -i "s:INSTALLDIR:${install_dir}:g" /home/webmaster/${install_dir}/conf/php/bokeh-pool.conf
+    fi
+
+    if [ ! -f /home/webmaster/${install_dir}/php/bokeh/local.php ]
+    then
+	echo "[BOKEH-PHP] Copy local.php conf file..."
+	cp /tmp/local.php /home/webmaster/${install_dir}/php/bokeh/
+	chown webmaster:webmaster /home/webmaster/${install_dir}/php/bokeh/local.php
+    fi
+    
+    if [ "${memcache_host}" == "none" ]
+    then
+	echo "[BOKEH-PHP] Disable PHP memcached server configuration..."
+        cp /tmp/session_save_path.dir /home/webmaster/${install_dir}/conf/php/session_save_path.conf
+        sed -i "s:^define('MEMCACHED://define('MEMCACHED:g" /home/webmaster/${install_dir}/php/bokeh/local.php
+    else
+	echo "[BOKEH-PHP] Enable PHP memcached server configuration..."
+        cp /tmp/session_save_path.memcache /home/webmaster/${install_dir}/conf/php/session_save_path.conf
+        sed -i "s:MEMCACHEHOST:${memcache_host}:g" /home/webmaster/${install_dir}/conf/php/session_save_path.conf
+        sed -i "s:MEMCACHEHOST:${memcache_host}:g" /home/webmaster/${install_dir}/php/bokeh/local.php
+    fi
+}
+
+
 install_dir=$1
-main
+memcache_host=$2
+
+ensure_directories
+ensure_install_utils
+ensure_bokeh_sources
+ensure_confs    
+
--- bokeh-php-prod/start-php.sh
+++ bokeh-php-prod/start-php.sh
@@ -1,38 +1,15 @@
 #!/bin/bash
 
 function main {
-	usermod -u ${uid} webmaster && groupmod -g ${uid} webmaster
-	chown webmaster:www-data /usr/local/lib/php/sessions
-	su -l webmaster -c "bash /tmp/init-php.sh ${install_dir}"
+    sed -i "s:INSTALLDIR:${install_dir}:g" /usr/local/etc/php/php.ini
+    sed -i "s:INSTALLDIR:${install_dir}:g" /usr/local/etc/php-fpm.conf
+    
+    usermod -u ${uid} webmaster && groupmod -g ${uid} webmaster
+    chown webmaster:www-data /usr/local/lib/php/sessions
+    su -l webmaster -c "bash /tmp/init-php.sh ${install_dir} ${memcache_host}"
 
-	if [ -z "$(ls -A /home/webmaster/${install_dir}/conf/php)" ]
-	then
-		echo "[BOKEH-PHP] Install PHP conf files..."
-		cp -p /tmp/bokeh-php-fpm.conf /home/webmaster/${install_dir}/conf/php/
-		cp -p /tmp/bokeh-pool.conf /home/webmaster/${install_dir}/conf/php/
-		sed -i "s:INSTALLDIR:${install_dir}:g" /home/webmaster/${install_dir}/conf/php/bokeh-php-fpm.conf
-		sed -i "s:INSTALLDIR:${install_dir}:g" /home/webmaster/${install_dir}/conf/php/bokeh-pool.conf
-	fi
-
-	if [ "${memcache_host}" == "none" ]
-	then
-		cp /tmp/session_save_path.dir /home/webmaster/${install_dir}/conf/php/session_save_path.conf
-		cp /tmp/local.php /home/webmaster/${install_dir}/php/bokeh/
-		chown webmaster:webmaster /home/webmaster/${install_dir}/php/bokeh/local.php
-		sed -i "s:^define('MEMCACHED://define('MEMCACHED:g" /home/webmaster/${install_dir}/php/bokeh/local.php
-	else
-		cp /tmp/session_save_path.memcache /home/webmaster/${install_dir}/conf/php/session_save_path.conf
-		sed -i "s:MEMCACHEHOST:${memcache_host}:g" /home/webmaster/${install_dir}/conf/php/session_save_path.conf
-		cp /tmp/local.php /home/webmaster/${install_dir}/php/bokeh/
-		chown webmaster:webmaster /home/webmaster/${install_dir}/php/bokeh/local.php
-		sed -i "s:MEMCACHEHOST:${memcache_host}:g" /home/webmaster/${install_dir}/php/bokeh/local.php
-	fi
-
-	sed -i "s:INSTALLDIR:${install_dir}:g" /usr/local/etc/php/php.ini
-	sed -i "s:INSTALLDIR:${install_dir}:g" /usr/local/etc/php-fpm.conf
-
-	echo "[BOKEH-PHP] Start php-fpm..."
-	php-fpm -F
+    echo "[BOKEH-PHP] Start php-fpm..."
+    php-fpm -F
 }
 
 main
