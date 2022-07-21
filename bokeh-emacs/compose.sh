e706a455e661eb3e7552e019a949f83bd7467d19
Laurent Laffont <llaffont@afi-sa.fr>
2022-07-21 17:15:03 +0200

    execute docker in another container

--- bokeh-emacs/compose.sh
+++ bokeh-emacs/compose.sh
@@ -3,6 +3,6 @@ export BOKEH_ROOT=$(pwd)
 export GROUP_ID=$(id -g)
 export USER_ID=$(id -u)
 export USERNAME=$(whoami)
-
+export DOCKER_GROUP_ID=$(cut -d: -f3 < <(getent group docker))
 cd $( dirname -- "${BASH_SOURCE[0]}" )
 docker-compose -f compose.yml ${*}
