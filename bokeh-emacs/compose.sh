f4e8c1ac0e4d9a97f42719045d346f67b54408a4
Patrick Barroca <pbarroca@afi-sa.fr>
2022-07-22 16:41:00 +0200

    bokeh-emacs composition include bokeh env

--- bokeh-emacs/compose.sh
+++ bokeh-emacs/compose.sh
@@ -4,5 +4,5 @@ export GROUP_ID=$(id -g)
 export USER_ID=$(id -u)
 export USERNAME=$(whoami)
 export DOCKER_GROUP_ID=$(cut -d: -f3 < <(getent group docker))
-cd $( dirname -- "${BASH_SOURCE[0]}" )
-docker-compose -f compose.yml ${*}
+cd $(dirname $(realpath $0))
+docker-compose --env-file $BOKEH_ROOT/docker/.env -f $BOKEH_ROOT/docker/compose.yml -f compose.yml ${*}
