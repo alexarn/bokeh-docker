5f2cc782b5febdf5818b49d8b96f16b06f5000e7
Patrick Barroca <pbarroca@afi-sa.fr>
2022-07-25 15:59:54 +0200

    bokeh emacs env vars written in .env.dev, port autodetected

--- bokeh-emacs/compose.sh
+++ bokeh-emacs/compose.sh
@@ -1,8 +1,42 @@
-#!/bin/sh
-export BOKEH_ROOT=$(pwd)
-export GROUP_ID=$(id -g)
-export USER_ID=$(id -u)
-export USERNAME=$(whoami)
-export DOCKER_GROUP_ID=$(cut -d: -f3 < <(getent group docker))
+#!/bin/bash
+
+function availablePort() {
+    local OPENED_PORTS=$(ss -lntu| awk '{split($5, array, ":"); print array[2]}')
+    local HTTP_PORT
+    for port_value in {10000..10500}
+    do
+        if [[ ! $(echo "$OPENED_PORTS" | grep ${port_value}) ]]
+        then
+            HTTP_PORT=$port_value; break
+        fi
+    done
+
+    echo $HTTP_PORT
+}
+
+BOKEH_ROOT=$(pwd)
+ENV_BASIC=$BOKEH_ROOT/docker/.env
+ENV_DEV=$BOKEH_ROOT/docker/.env.dev
+
+if [ ! -f $ENV_DEV ]
+then
+    cp $ENV_BASIC $ENV_DEV
+    WEB_PORT=$(availablePort)
+    if [ -z "${WEB_PORT}" ]
+    then
+	info "no HTTP port left"
+	exit 2
+    fi
+
+    sed -i "s:WEB_PORT=.*:WEB_PORT=$WEB_PORT:" $ENV_DEV
+
+    echo "" >> $ENV_DEV
+    echo "BOKEH_ROOT=$BOKEH_ROOT" >> $ENV_DEV
+    echo "GROUP_ID=$(id -g)" >> $ENV_DEV
+    echo "USER_ID=$(id -u)" >> $ENV_DEV
+    echo "USERNAME=$(whoami)" >> $ENV_DEV
+    echo "DOCKER_GROUP_ID=$(cut -d: -f3 < <(getent group docker))" >> $ENV_DEV
+fi
+
 cd $(dirname $(realpath $0))
-docker-compose --env-file $BOKEH_ROOT/docker/.env -f $BOKEH_ROOT/docker/compose.yml -f compose.yml ${*}
+docker-compose --env-file $ENV_DEV -f $BOKEH_ROOT/docker/compose.yml -f compose.yml ${*}
